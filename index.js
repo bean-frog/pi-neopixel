85b904ce250b2617e099531a41cffbf4c52a4ec9
bean-frog <gkieran32855@gmail.com>
2024-05-21 18:22:40 -0700

    i think i did stuff

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+const { StripType, ws281x } = require('piixel');
+const leds = 36
+ws281x.configure(
+	{
+		gpio: 18,
+		leds: leds,
+		type: StripType.WS2811_STRIP_GRB,
+		resetOnExit: true
+	}
+)
+const pixels = new Uint32Array(leds);
+
+function setColor({ lednum, color }) {
+
+  const { r, g, b } = color;
+
+  if (lednum >= 0 && lednum < leds) {
+
+    pixels[lednum] = (r << 8) | (g << 16) | b;
+
+    ws281x.render(pixels);
+
+  } else {
+
+    console.error('LED number out of range');
+
+  }
+
+}
+
 const express = require('express');
 const path = require('path');
 
@@ -15,7 +45,7 @@ const port = 3000;
 app.use(express.json());
 
 app.post('/led', (req, res) => {
-  
+  setColor(req.body)
   console.log(req.body);
 });
 
